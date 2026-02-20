487b38e82d6b60d5adfbff6e7430751a0bee712d
Sean Murphy <sean@iamseanmurphy.com>
2026-02-20 19:26:02 +0000

    2.1.30

--- blocksy/inc/options/meta/page.php
+++ blocksy/inc/options/meta/page.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Page meta options.
+ *
+ * Note: Meta values from these options are sanitized in blocksy_sanitize_post_meta_options()
+ * located in admin/helpers/validator.php. When adding new options that accept
+ * user input (typography, gradients, etc.), ensure they are handled there.
+ *
+ * @see blocksy_sanitize_post_meta_options()
+ */
 
 $maybe_taxonomy = blocksy_maybe_get_matching_taxonomy('page', false);
 
